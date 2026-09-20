771ef4d1721cfda2b0130140cb6125f048b33cc8
Lucas Mendes <lucasrafaelmendes8@gmail.com>
2026-09-20 19:44:45 -0300

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-projetos/',
   plugins: [vue()],
 })
